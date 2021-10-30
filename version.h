f20b8a1d99819532fa6752a5624236f59157eb95
Per Salmi <per.salmi@gmail.com>
2021-10-30 22:20:53 +0200

    Update to VS 2022 format.

---
+++ version.h
@@ -0,0 +1,5 @@
+#define NSSM_VERSION _T("2.24-113-ge8211e9")
+#define NSSM_VERSIONINFO 2,24,113,0
+#define NSSM_DATE _T("2021-10-30")
+#define NSSM_FILEFLAGS VS_FF_PRERELEASE
+#define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-")
